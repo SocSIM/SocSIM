400f78acf251286b424858466743c492fb505ef1
Dominik Stańczak <stanczakdominik@gmail.com>
2020-01-30 14:54:04 +0100

    Add test for deterministic result

--- SOC/models/tests/test_btw.py
+++ SOC/models/tests/test_btw.py
@@ -10,3 +10,20 @@ def test_boundary_shape():
 def test_run():
     sim = BTW(10)
     sim.run(10)
+
+def test_deterministic_result():
+    b = BTW(5, save_every = 1)
+
+    b.values[...] = 3
+    b.values[3, 3] += 1
+
+    b.topple_dissipate()
+    output = [[3, 4, 4, 4, 4, 4, 3],
+              [4, 1, 3, 3, 3, 1, 4],
+              [4, 3, 1, 3, 1, 3, 4],
+              [4, 3, 3, 0, 3, 3, 4],
+              [4, 3, 1, 3, 1, 3, 4],
+              [4, 1, 3, 3, 3, 1, 4],
+              [3, 4, 4, 4, 4, 4, 3],]
+    np.testing.assert_allclose(b.values, output)
+
