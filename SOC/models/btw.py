929502117c72b4910e1b1941cb796dc852bd3bec
Oleg Kmechak <ofcrashbash@gmail.com>
2020-01-30 14:24:32 +0100

    BTW avalanche loop fix, new observables add

--- SOC/models/btw.py
+++ SOC/models/btw.py
@@ -16,7 +16,7 @@ class BTW(common.Simulation):
         super().__init__(*args, **kwargs)
         self.d = 2 #lattice dimmension 
         self.q = 2*self.d #grains amount used at driving 
-        self.z_c = self.q #critical slope
+        self.z_c = self.q - 1 #critical slope
         self.values = np.zeros((self.L_with_boundary, self.L_with_boundary), dtype=int)
 
     def adjacent_indexes(self, x, y):
@@ -37,29 +37,23 @@ class BTW(common.Simulation):
         """
         location = np.random.randint(self.BOUNDARY_SIZE, self.L_with_boundary - 1, size = (num_particles, 2))
         for x, y in location:
-            val = self.values[x, y]
             self.values[x, y] += 1
-            for x_ad, y_ad in self.adjacent_indexes(x, y):
-                if self.values[x_ad, y_ad] > val:
-                    self.values[x_ad, y_ad] - 1
 
-    def topple_dissipate(self) -> bool:
+    def topple_dissipate(self) -> int:
         """
         Distribute material from overloaded sites to neighbors.
 
         Convenience wrapper for the numba.njitted `topple` function defined in `manna.py`.
 
-        :rtype: bool
+        :rtype: int
         """
 
-
-
-        return topple(self.values, self.visited, self.z_c, self.BOUNDARY_SIZE)    
+        return topple(self.values, self.visited, self.releases, self.z_c, self.BOUNDARY_SIZE)    
 
 
 
 @numba.njit
-def topple(values: np.ndarray, visited: np.ndarray, critical_value: int, boundary_size: int) -> bool:
+def topple(values: np.ndarray, visited: np.ndarray, releases: np.ndarray, critical_value: int, boundary_size: int) -> int:
     """
     Distribute material from overloaded sites to neighbors.
 
@@ -69,34 +63,38 @@ def topple(values: np.ndarray, visited: np.ndarray, critical_value: int, boundar
     :type values: np.ndarray
     :param visited: boolean array, needs to be cleaned beforehand
     :type visited: np.ndarray
+    :param releases: boolean array, used to evalute number of sandpiles activations
+    :type releases: np.ndarray
     :param critical_value: nodes topple above this value
     :type critical_value: int
     :param boundary_size: size of boundary for the array
     :type boundary_size: int
-    :rtype: bool
+    :rtype: int
     """
 
     # find a boolean array of active (overloaded) sites
-    active_sites = common.clean_boundary_inplace(values >= critical_value, boundary_size)
-    # odrzucam 
-
-
-    if active_sites.any():
+    number_of_iterations = 0
+    active_sites = common.clean_boundary_inplace(values > critical_value, boundary_size)
+    
+    while active_sites.any():
+        releases += active_sites
+        
         indices = np.vstack(np.where(active_sites)).T
         # a Nx2 array of integer indices for overloaded sites
-        N = indices.shape[0]
 
+        N = indices.shape[0]
         for i in range(N):
             x, y = indices[i]
-
-            values[x, y] -= critical_value # zależy od parametru?
+            values[x, y] -= critical_value + 1 # zależy od parametru?
 
             neighbors = np.array([(x - 1, y), (x, y - 1), (x + 1, y), (x, y + 1)])
-
             for j in range(len(neighbors)):
                 xn, yn = neighbors[j]
                 values[xn, yn] += 1
                 visited[xn, yn] = True
-        return True
-    else:
-        return False # nothing happened, we can stop toppling
+        
+        number_of_iterations += 1
+
+        active_sites = common.clean_boundary_inplace(values > critical_value, boundary_size)
+
+    return number_of_iterations
